91fe286482cf0b991549a35b96424dc5afb40916
yago.monteiroo@gmail.com <yago.monteiroo@gmail.com>
2021-09-06 20:08:43 -0300

    .env

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 app.use(express.json());
 
 const Conn = require('./models/conn/conn');
+const db_url = process.env.DB_URL;
+const db_user= process.env.DB_USER;
+const db_pass= process.env.DB_PASS;
+const db_data= process.env.DB_DATA;
 
-Conn("localhost",27017,"musicas");
+
+Conn(db_url,db_user,db_pass,db_data);
 
 const port = 3000;
 
